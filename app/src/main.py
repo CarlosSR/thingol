8bc02eeea792cb5b56b69d7b8c2ac391aa49f35e
Carlos <iscarlosrodriguez@gmail.com>
2023-04-05 10:56:04 -0600

    I going to save this and then use flask-jwt-extended

--- app/src/main.py
+++ app/src/main.py
@@ -1,10 +1,36 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session, flash
+import jwt
 from services import users as UserService
+from functools import wraps
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '1234567890'
+
+
+def token_required(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+        if not token:
+            return jsonify({'Error': 'Token is missing.'}), 401
+
+        try:
+            data = jwt.decode(token, '1234567890', algorithms=['HS256', ])
+        except jwt.InvalidTokenError:
+            return jsonify({'Message': 'Invalid token.'}), 403
+        return func(*args, **kwargs)
+    return decorated
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    result = UserService.login(data)
+    return jsonify(result)
 
 
 @app.route("/index")
+@token_required
 def index():
     service = UserService
     result = service.all()
--- app/src/services/users.py
+++ app/src/services/users.py
@@ -1,4 +1,9 @@
 from app.src.dbconn import mariadb as conn
+from app.src.main import app as app
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+from datetime import datetime, timedelta
+from flask import make_response
 
 
 def all():
@@ -17,11 +22,38 @@ def create(data):
     first_name = data['first_name']
     last_name = data['last_name']
     password = data['password']
-    insert = f"INSERT INTO users " \
-             f"(names, first_name, last_name, password) " \
-             f"values " \
-             f"('{names}', '{first_name}', '{last_name}', '{password}'); "
+    email = data['email']
+    hashed_pass = generate_password_hash(password)
+    insert = f'''INSERT INTO users 
+                    (names, first_name, last_name, email, password) 
+                values
+                    ('{names}', '{first_name}', '{last_name}', '{email}', '{hashed_pass}');'''
     cursor = db.cursor()
     cursor.execute(insert)
     db.commit()
     return 'user saved.'
+
+
+def login(data):
+    db = conn()
+    email = data['email']
+    password = data['password']
+    query = f"SELECT * FROM users WHERE email = '{email}'"
+    cursor = db.cursor(dictionary=True)
+    cursor.execute(query)
+    user = cursor.fetchone()
+    if check_password_hash(user['password'], password):
+        token = jwt.encode({
+            'user': user.get('first_name'),
+            'exp': str(datetime.utcnow() + timedelta(seconds=1800)),
+        }, '1234567890')
+        return {'token': token}
+    else:
+        return 'Unable to verify', 403
+
+
+
+
+
+
+
